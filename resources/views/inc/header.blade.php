3bdffe12162712da42c90a6a5dcaf033215671d1
NyAvoFandresena <hnafdsn380095@gmail.com>
2024-01-06 10:24:39 +0300

    first commit

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,11 @@
+<!-- component -->
+<header>
+    <section class="bg-center bg-no-repeat bg-[url({{ asset('images/tana.png') }})] bg-gray-700 bg-blend-multiply">
+        <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                Juriste Produir</h1>
+            <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Pour une ville plus saine
+            </p>
+        </div>
+    </section>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Juriste Produir</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        :root {}
+
+        body {
+            box-sizing: border-box;
+            margin: 0;
+        }
+    </style>
+</head>
+
+<body>
+    @include('inc.header')
+    @yield('content')
+    @include('inc.footer')
+</body>
+
+</html>
