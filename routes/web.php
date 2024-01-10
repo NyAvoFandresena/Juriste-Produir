559ac5cd731c0bd1c765af8750bc60a41c6cfd85
NyAvoFandresena <hnafdsn380095@gmail.com>
2024-01-10 08:45:38 +0300

    aster
    ain

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,21 @@ Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 // LOGS
 Route::post('/logout', [LogoutController::class,'logout'])->name('logout');
 
-// ADMIN
-Route::get('/admin/dashboard', [AdminPagesController::class, 'index'])->name('admin.dashboard');
-Route::get('/admin/interns', [AdminPagesController::class, 'internList'])->name('admin.interns');
-Route::get('/admin/intern/1', [AdminPagesController::class, 'internInfo'])->name('admin.intern');
-Route::get('/admin/task/1', [AdminPagesController::class, 'taskInfo'])->name('admin.task');
 
-// INTERNE
-Route::get('/intern/dashboard', [InternPagesController::class, 'index'])->name('intern.dashboard');
-Route::get('/intern/create', [InternPagesController::class,'create'])->name('intern.create');
-Route::get('/intern/modify/1', [InternPagesController::class,'modify'])->name('intern.modify');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    // ADMIN
+    Route::get('/admin/dashboard', [AdminPagesController::class, 'index'])->name('admin.dashboard');
+    Route::get('/admin/interns', [AdminPagesController::class, 'internList'])->name('admin.interns');
+    Route::get('/admin/intern/1', [AdminPagesController::class, 'internInfo'])->name('admin.intern');
+    Route::get('/admin/task/1', [AdminPagesController::class, 'taskInfo'])->name('admin.task');
+});
+
+Route::middleware(['auth', 'role:intern'])->group(function () {
+    // INTERNE
+    Route::get('/dashboard', [InternPagesController::class, 'index'])->name('intern.dashboard');
+    Route::get('/create', [InternPagesController::class,'create'])->name('intern.create');
+    Route::get('/modify/1', [InternPagesController::class,'modify'])->name('intern.modify');
+});
+
 
 require __DIR__.'/auth.php';
